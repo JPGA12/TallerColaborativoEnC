c02fe7e43968912b2de2988dcaa3ac68bdf49308
Sebastian <leon.4615@hotmail.com>
2022-05-21 00:54:47 -0500

    version 2.0

--- main.c
+++ main.c
@@ -146,6 +146,143 @@ char* newYear() {
     return string;
 }
 
+char *interseccion(char cadena[], char cadena2[]) {
+    int tamano1, tamano2, cont = 0, contador = 0;
+    char aux[255], aux2[255];
+    char *cadenaSalida = "";
+
+    tamano2 = strlen(cadena2);
+    for (int i = tamano2 - 1; i >= 0; --i) {
+        for (int j = 0; j < tamano2 - 1; ++j) {
+            if (cadena2[i] == cadena2[j]) {
+                cont++;
+            }
+        }
+        if (cont > 1) {
+            memset(aux, 0, 255);
+            memset(aux2, 0, 255);
+
+            for (int j = i + 1; j < tamano2 - 1; ++j) {
+                aux[contador] = cadena2[j];
+                contador++;
+            }
+            contador = 0;
+            for (int j = 0; j < i; ++j) {
+                aux2[j] = cadena2[j];
+            }
+            memset(cadena2, 0, 255);
+            strcat(strcpy(cadena2, aux2), aux);
+
+            tamano2 = strlen(cadena2);
+        }
+        cont = 0;
+    }
+    tamano1 = strlen(cadena);
+    tamano2 = strlen(cadena2);
+    memset(aux, 0, 255);
+    cont = 0;
+    contador = 0;
+    for (int i = 0; i < tamano2; ++i) {
+        for (int j = 0; j < tamano1; ++j) {
+            if (cadena2[i] == cadena[j]) {
+                cont++;
+            }
+        }
+        if (cont > 0) {
+            aux[contador] = cadena2[i];
+            contador++;
+            cont = 0;
+        }
+    }
+
+    cadenaSalida = aux;
+    return cadenaSalida;
+}
+
+char* deleteRepeat(char palabra[],char *resultado){
+    int i=0,j=0;
+    while(palabra[i])
+    {
+        if(!strchr(resultado,palabra[i]))
+            resultado[j++]=palabra[i];
+        i++;
+    }
+    resultado[j]='\0';
+    printf("\nEl resultado seria!\n%s",resultado);
+
+    return NULL;
+}
+
+char *desplazamiento(char *palabra,int indice,int nVeces,int direccion){
+    char aux[strlen(palabra)],aux2;
+    int cont = 0,cont2 = indice,i=0;
+    if(direccion == 1){
+        while (i < nVeces){
+            if(cont2 == 0){
+                cont2= strlen(palabra)-1;
+            } else{
+                cont2--;
+            }
+            i++;
+        }
+
+
+        printf("%d\n",cont2);
+        aux2 = palabra[indice];
+        printf("%c\n",aux2);
+
+        for (int i = 0; i <= strlen(palabra); ++i) {
+            if(cont2 == i){
+                aux[cont]=aux2;
+                cont++;
+                aux[cont] = palabra[i];
+                cont++;
+            }else if(indice != i){
+                aux[cont]=palabra[i];
+                cont++;
+            } else{
+                aux[i]=palabra[i-1];
+            }
+        }
+        printf("%s\n",&aux);
+
+    } else if(direccion == 2){
+        while (i < nVeces){
+            if(cont2 == strlen(palabra)-1){
+                cont2=0;
+            } else{
+                cont2++;
+            }
+            i++;
+        }
+
+        cont2++;
+
+        printf("%d\n",cont2);
+        aux2 = palabra[indice];
+        printf("%c\n",aux2);
+
+        for (int i = 0; i <= strlen(palabra); ++i) {
+            if(cont2 == i){
+                aux[cont]=aux2;
+                cont++;
+                aux[cont] = palabra[i];
+                cont++;
+            }else if(indice != i){
+                aux[cont]=palabra[i];
+                cont++;
+            } else{
+                aux[i]=palabra[i+1];
+            }
+        }
+        printf("%s\n",&aux);
+
+    }else{
+        printf("error");
+    }
+
+}
+
 
 int main() {
 
@@ -239,13 +376,51 @@ int main() {
 
                 break;
             case 7:
-                printf("Interseccion\n");
+                printf("\n>>Diferencia entre dos cadenas<<");
+
+                char cadena7[255], cadena27[255];
+
+                printf("\nDigite la cadena de texto uno...");
+                fgets(cadena7, 255, stdin);
+
+                printf("\nDigite la cadena de texto dos...");
+                fgets(cadena27, 255, stdin);
+
+                printf("\nLa Interseccion es: %s \n", interseccion(cadena7, cadena27));
+                printf("\nPresione una tecla para continuar . . .");
+                getchar();
                 break;
             case 8:
-                printf("Eliminar repetidos\n");
+                printf("\n>>Eliminar  repetidos<<");
+                char cadena6[255], caracter1[1];
+                printf("\nIngrese la cadena...");
+                gets(cadena6);
+
+                printf("\n%s \n", deleteRepeat(cadena6, caracter1));
+
+                printf("\nPresione una tecla para continuar . . .");
+                getchar();
                 break;
             case 9:
-                printf("Desplazamiento\n");
+                printf("\n HA SELECCIONADO LA OPCION 9: desplazamiento \n\nIngrese u oracion una palabra para desplazar un elemento de este\n");
+                char words[50];
+                int y,y1,x1;
+                //scanf("%s",words);
+                fgets(words,500,stdin );
+                printf("%s",words);
+                printf("\nIngrese el indice del caracter que desea desplazar (el indice empieza a contar desde 0 de izquierda a derecha)\n");
+                scanf("%d",&y);
+                printf("\nIngrese cuantas posiciones se desplazara el caracter\n");
+                scanf("%d",&y1);
+                fflush(stdin);
+                printf("\nIngrese hacia donde va a insertar el caracter ( 1 izquierda o 2 derecha)\n");
+                scanf("%d",&x1);
+                while (x1 != 1 && x1 != 2){
+                    printf("error, seleccione una opcion valida\n");
+                    printf("\nIngrese hacia donde va a insertar el caracter ( 1 izquierda o 2 derecha)\n");
+                    scanf("%d",&x1);
+                }
+                desplazamiento(words,y,y1,x1);
                 break;
             case 10:
                 printf("Verificacion de E-mail \n");
